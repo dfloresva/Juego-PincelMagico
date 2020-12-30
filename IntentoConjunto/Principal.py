0762249a14423ab91b5a08ee25bd8b3f940892ba
dfloresva <dfloresva@unsa.edu.pe>
2020-12-30 12:54:27 -0500

    temrino proyecto fase 1

--- IntentoConjunto/Principal.py
+++ IntentoConjunto/Principal.py
@@ -5,13 +5,14 @@ import numpy as np
 def PizarraVirtual():	
 	try:
 		cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)	#captura imagen de la camara web
-		cont=0	#captura imagen de la camara web
-		capTomada=False
-		AzulBajo = np.array([75, 115, 88], np.uint8)
-		AzulAlto = np.array([112,255, 255], np.uint8)
+		cont=0	#contador de captura de imagen
+		capTomada=False	#controlador de 1 sola captura por vez
+		AzulBajo = np.array([75, 115, 88], np.uint8)	#valor minimo de reconocimiento de color
+		AzulAlto = np.array([112,255, 255], np.uint8)	#valor maximo de reconocimiento de color
 
 		# Colores para pintar
 		colorAzul = (255,41,36)
+		colorBlanco=(255,255,255)
 		colorAmarillo = (89,222,255)
 		colorRosa = (128,0,255)
 		colorVerde = (0,255,36)
@@ -19,10 +20,11 @@ def PizarraVirtual():
 		colorTomarCaptura = (105,22,214) # Solo se usará para el cuadro superior de 'Limpiar Pantalla'
 
 		# Grosor de línea recuadros superior izquierda (color a dibujar)
-		grosorAzul = 4
+		grosorAzul = 3
 		grosorAmarillo = 3
 		grosorRosa = 3
 		grosorVerde = 3
+		grosorBlanco = 3
 
 		# Grosor de línea recuadros superior derecha (grosor del marcador para dibujar)
 		grosorPeque = 6
@@ -30,7 +32,7 @@ def PizarraVirtual():
 		grosorGrande = 6
 
 		#--------------------- Variables para el marcador / lápiz virtual -------------------------
-		color = colorAzul  # Color de entrada, y variable que asignará el color del marcador
+		color = colorBlanco  # Color de entrada, y variable que asignará el color del marcador
 		grosor = 4 # Grosor que tendrá el marcador
 		#------------------------------------------------------------------------------------------
 
@@ -39,8 +41,6 @@ def PizarraVirtual():
 		imAux = None
 		concat=None
 		while True:
-			#if contador%100==0:
-				#ingresoAudio()
 			ret,frame = cap.read()
 			if ret==False: 
 				break
@@ -56,6 +56,8 @@ def PizarraVirtual():
 			cv2.rectangle(frame,(55,0),(105,50),colorRosa,grosorRosa)
 			cv2.rectangle(frame,(110,0),(155,50),colorVerde,grosorVerde)
 			cv2.rectangle(frame,(160,0),(205,50),colorAzul,grosorAzul)
+			cv2.rectangle(frame,(210,0),(255,50),colorBlanco,grosorBlanco)
+			
 			##Cambios que tendria que hacer Benjaz
 			
 			# Rectángulo superior central, que nos ayudará a limpiar la pantalla
@@ -65,10 +67,10 @@ def PizarraVirtual():
 
 
 			#------------------------ Sección Derecha ------------------------------------------
-			cv2.rectangle(frame,(530,70),(635,200),colorTomarCaptura,3)
-			cv2.putText(frame,'Captura',(550,130),7,0.7,colorTomarCaptura,1,cv2.LINE_AA)
-			cv2.putText(frame,'Pantalla',(550,150),6,0.6,colorTomarCaptura,1,cv2.LINE_AA)
-
+			
+			cv2.rectangle(frame,(530,200),(635,350),colorTomarCaptura,3)
+			cv2.putText(frame,'Captura',(550,270),7,0.7,colorTomarCaptura,1,cv2.LINE_AA)
+			cv2.putText(frame,'Pantalla',(550,290),6,0.6,colorTomarCaptura,1,cv2.LINE_AA)
 			
 
 			# Cuadrados dibujados en la parte superior derecha (grosor del marcador para dibujar)
@@ -101,24 +103,38 @@ def PizarraVirtual():
 							grosorRosa = 2
 							grosorVerde = 2
 							grosorAzul = 2
+							grosorBlanco = 2
 						if 55 < x2 < 105 and 0 < y2 < 50:
 							color = colorRosa # Color del lápiz/marcador virtual
 							grosorAmarillo = 2
 							grosorRosa = 6
 							grosorVerde = 2
 							grosorAzul = 2
+							grosorBlanco = 2
 						if 110 < x2 < 155 and 0 < y2 < 50:
 							color = colorVerde # Color del lápiz/marcador virtual
 							grosorAmarillo = 2
 							grosorRosa = 2
 							grosorVerde = 6
 							grosorAzul = 2
+							grosorBlanco = 2
 						if 160 < x2 < 205 and 0 < y2 < 50:
 							color = colorAzul # Color del lápiz/marcador virtual
 							grosorAmarillo = 2
 							grosorRosa = 2
 							grosorVerde = 2
 							grosorAzul = 6
+							grosorBlanco = 2
+						
+						if 210 < x2 < 255 and 0 < y2 < 50:
+    						
+							color = colorBlanco # Color del lápiz/marcador virtual
+							grosorAmarillo = 2
+							grosorRosa = 2
+							grosorVerde = 2
+							grosorAzul = 2
+							grosorBlanco = 6
+
 						if 490 < x2 < 540 and 0 < y2 < 50:
 							grosor = 3 # Grosor del lápiz/marcador virtual
 							grosorPeque = 6
@@ -139,43 +155,42 @@ def PizarraVirtual():
 							cv2.putText(frame,'Limpiar',(320,20),6,0.6,colorLimpiarPantalla,2,cv2.LINE_AA)
 							cv2.putText(frame,'pantalla',(320,40),6,0.6,colorLimpiarPantalla,2,cv2.LINE_AA)
 							imAux = np.zeros(frame.shape,dtype=np.uint8)
-						##################
-						try:						
-							if (530 <= x2 <= 635 and 70 <= y2 <= 200 ):
-								if capTomada==False:
-									cv2.imwrite('captura'+str(cont)+'.png',concat)
-									capTomada=True
-									cont=cont+1
-								if capTomada==True:
-    									pass
-							else:
-								capTomada=False
-							#capTomada=True 
-						except:
-
-							print("falla catastrofica")
-						###################
 						if 0 < y2 < 60 or 0 < y1 < 60 :
 							imAux = imAux						
 						else:
 							imAux = cv2.line(imAux,(x1,y1),(x2,y2),color,grosor)
+						##################
+						try:						
+							if (530 < x2 < 635 and 200 < y2 < 350 ):
+								if capTomada==False:									
+									cv2.imwrite('captura'+str(cont)+'.png',concat)									
+									capTomada=True
+									cont=cont+1										
+								if capTomada==True:
+										pass
+							else:
+								capTomada=False
+							#capTomada=True 
+						except:
+							print("falla catastrofica")
+						###################
+
 							
 					cv2.circle(frame,(x2,y2),grosor,color,2)
 					x1 = x2
 					y1 = y2
 				else:
-					x1, y1 = None, None		
+					x1, y1 = None, None	
 			imAuxGray = cv2.cvtColor(imAux,cv2.COLOR_BGR2GRAY)
 			_, th = cv2.threshold(imAuxGray,10,255,cv2.THRESH_BINARY)
 			thInv = cv2.bitwise_not(th)
 			frame = cv2.bitwise_and(frame,frame,mask=thInv)
 			frame = cv2.add(frame,imAux)		
 
+			#panel principal concatenacion de 2 frame
 			concat=cv2.hconcat([frame,imAux])
 			cv2.imshow('Juego: Pincel Magico',concat)
-			#cv2.imshow('imAux',imAux)
-			#cv2.imshow('frame', frame)
-			
+	
 			k = cv2.waitKey(1)
 			if k == 27:
 				break
@@ -183,4 +198,4 @@ def PizarraVirtual():
 		return "lol"
 	cap.release()
 	cv2.destroyAllWindows()
-#PizarraVirtual()
+PizarraVirtual()
